52e72aa87677bc5d6b7556e44e3051450b87c040
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-02 22:48:05 -0300

    Construindo por parâmetro // 2. Constructor e Renderização => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -1,42 +1,4 @@
 import { Personagem } from "./modules/personagem.js"
 
-// const personagemPedrinho = {
-//     nome: 'Pedrinho',
-//     vida: 7,
-//     mana: 12,
-//     level: 5,
-//     tipo: 'Mago',
-// }
-
-// const personagemJose = {
-//     nome: 'Jose',
-//     vida: 7,
-//     mana: 6,
-//     level: 3,
-//     tipo: 'Arqueiro',
-// }
-
-const personagemAna = {
-    nome: 'Ana',
-    vida: 8,
-    mana: 10
-}
-
-const personagemPedrinho = new Personagem()
-personagemPedrinho.nome = 'Pedrinho'
-personagemPedrinho.mana = 12
-personagemPedrinho.vida = 7
-personagemPedrinho.level = 5
-personagemPedrinho.tipo = 'Mago'
-
-const personagemJose = new Personagem()
-personagemJose.nome = 'Jose'
-personagemJose.mana = 6
-personagemJose.vida = 7
-personagemJose.level = 3
-personagemJose.tipo = 'Arqueiro'
-
-
-
-console.log('Insignia de ' + personagemPedrinho.nome + ': ' + personagemPedrinho.obterInsignia());
-console.log('Insignia de ' + personagemJose.nome + ': ' + personagemJose.obterInsignia());
+const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
