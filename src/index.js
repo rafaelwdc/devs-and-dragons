ad44608fc1013d62c25c8d8c474339285ef5b8c9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 19:16:15 -0300

    Desafio: Classe Mago // 2. Constructor e Renderização => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
+import { Mago } from "./modules/mago.js"
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
@@ -9,3 +10,4 @@ const personagens = [personagemPedrinho, personagemJose];
 new PersonagemView(personagens).render()
 
 console.log(personagemJose.vida);
+console.log(new Mago('Gelo', 7, 9));
