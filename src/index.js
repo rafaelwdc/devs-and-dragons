83a71ed54633291b9ed522ef1e75a8bd3de64c3b
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 20:06:53 -0300

    Sobrescrevendo métodos && Desafio: Classe Arqueiro // 3. Fundamentos da POO => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { PersonagemView } from "./components/personagem-view.js";
+import { Arqueiro } from "./modules/arqueiro.js";
 import { Mago } from "./modules/mago.js";
 
-const magoPatricky = new Mago('Patricky', 4, 'Fogo', 4, 3);
-const magaCamila = new Mago('Camila', 8, 'Gelo', 7, 10);
+const magaCamila = new Mago('Camila', 4, 'Fogo', 4, 3);
+const magoPatricky = new Mago('Patricky', 8, 'Gelo', 7, 10);
+const arqueiroRafael = new Arqueiro('Rafael', 7, 8)
 
-const personagens = [magoPatricky, magaCamila];
+const personagens = [magoPatricky, magaCamila, arqueiroRafael];
 
 new PersonagemView(personagens).render();
--- src/modules/mago.js
+++ src/modules/mago.js
@@ -12,4 +12,11 @@ export class Mago extends Personagem {
         this.levelMagico = levelMagico
         this.inteligencia = inteligencia
     }
+
+    obterInsignia() {
+        if(this.levelMagico >= 5 && this.inteligencia >= 5) {
+            return `Mestre do ${this.elementoMagico}`
+        }
+        return super.obterInsignia();
+    }
 }
