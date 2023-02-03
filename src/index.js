68d949d25dfbf9ee96e847a899f95ced837383c7
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 20:35:29 -0300

    Composição // 3. Fundamentos da POO => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { PersonagemView } from "./components/personagem-view.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
 import { Mago } from "./modules/mago.js";
 
 const magaCamila = new Mago('Camila', 4, 'Fogo', 4, 3);
 const magoPatricky = new Mago('Patricky', 8, 'Gelo', 7, 10);
-const arqueiroRafael = new Arqueiro('Rafael', 7, 8)
+const arqueiroRafael = new Arqueiro('Rafael', 7, 8);
+const arqueiroMagaSteela = new ArqueiroMago('Steela', 7, 10, 'Ar', 4, 8)
 
-const personagens = [magoPatricky, magaCamila, arqueiroRafael];
+const personagens = [magoPatricky, magaCamila, arqueiroRafael, arqueiroMagaSteela];
 
 new PersonagemView(personagens).render();
