67d782d0ae644c02de29417d0bba37e8bb64ca4d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 21:33:21 -0300

    Getter // 5. Encapsulamento => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { PersonagemView } from "./components/personagem-view.js";
 import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
 import { Mago } from "./modules/mago.js";
-import { Personagem } from "./modules/personagem.js";
+// import { Personagem } from "./modules/personagem.js";
 
 const magaCamila = new Mago('Camila', 4, 'Fogo', 4, 3);
 const magoPatricky = new Mago('Patricky', 8, 'Gelo', 7, 10);
