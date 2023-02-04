eab8e15c893a009b40063555850c2cfcb085e8a2
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 21:41:17 -0300

    Desafio: Classe Guerreiro // 5. Encapsulamento => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { PersonagemView } from "./components/personagem-view.js";
 import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 import { Arqueiro } from "./modules/arqueiro.js";
+import { Guerreiro } from "./modules/guerreiro.js";
 import { Mago } from "./modules/mago.js";
 // import { Personagem } from "./modules/personagem.js";
 
@@ -8,8 +9,9 @@ const magaCamila = new Mago('Camila', 4, 'Fogo', 4, 3);
 const magoPatricky = new Mago('Patricky', 8, 'Gelo', 7, 10);
 const arqueiroRafael = new Arqueiro('Rafael', 7, 8);
 const arqueiroMagaSteela = new ArqueiroMago('Steela', 7, 10, 'Ar', 4, 8);
+const guerreiraHirro = new Guerreiro('HiroD', 8); 
 
-const personagens = [magoPatricky, magaCamila, arqueiroRafael, arqueiroMagaSteela];
+const personagens = [magoPatricky, magaCamila, arqueiroRafael, arqueiroMagaSteela, guerreiraHirro];
 
 new PersonagemView(personagens).render();
 
