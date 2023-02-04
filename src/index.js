5495c4db43064c1783b20c3644f5698620077ef0
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-03 21:27:09 -0300

    Propriedades privadas // 5. Encapsulamento => JavaScript: Classes e Heranças no desenvolvimenro de aplicações com Orientação a Objetos

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import { Personagem } from "./modules/personagem.js";
 const magaCamila = new Mago('Camila', 4, 'Fogo', 4, 3);
 const magoPatricky = new Mago('Patricky', 8, 'Gelo', 7, 10);
 const arqueiroRafael = new Arqueiro('Rafael', 7, 8);
-const arqueiroMagaSteela = new ArqueiroMago('Steela', 7, 10, 'Ar', 4, 8)
+const arqueiroMagaSteela = new ArqueiroMago('Steela', 7, 10, 'Ar', 4, 8);
 
 const personagens = [magoPatricky, magaCamila, arqueiroRafael, arqueiroMagaSteela];
 
 new PersonagemView(personagens).render();
 
-console.log(Personagem.verificarVencedor(arqueiroRafael, magaCamila));
+// console.log(Personagem.verificarVencedor(arqueiroRafael, magaCamila));
+console.log(arqueiroRafael);
